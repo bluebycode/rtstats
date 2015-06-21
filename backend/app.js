0bccc108bc8d35dda48517e4141685c1ddcc6bd6
vrandkode <vrandkode@gmail.com>
2015-06-21 18:16:18 +0200

    allowing send broadcast updates to every client

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({ port: 8001 }),
     _ = require('underscore');
 
-var util   = require('util');
+var util  = require('util');
+
 
 wss.on('connection', function connection(ws) {
-  console.log('connected!!');
+
+  console.log('connected!!',ws);
 
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
@@ -19,7 +21,7 @@ wss.on('connection', function connection(ws) {
       var result = _.map(data, function(e){
         return [e[0],e[1]+beginning];
       });
-      console.log('sending data with factor:', beginning, result);
+      //console.log('sending data with factor:', beginning, result);
       ws.send(JSON.stringify(eventObject('dau', result)));
     }catch(er){
       console.log(er);
@@ -34,3 +36,17 @@ wss.on('connection', function connection(ws) {
   };
 });
 
+setInterval(function(){
+  console.log('sending broadcast data');
+  wss.broadcast = function broadcast(data) {
+    var i = 0;
+    wss.clients.forEach(function each(client) {
+      console.log('sending to client ', i++);
+      //client.send(data);
+    });
+  };
+  wss.broadcast(JSON.stringify({
+      event: 'daily',
+      data: [ [ 1434585600000 , 10] , [ 1434672000000 , 256] ,[ 1434758400000 , 100] , [ 1434844800000 , 156] ]
+    }));
+},2000);
