5eed696061545f7fb848f261962ac50ccb58676f
vrandkode <vrandkode@gmail.com>
2015-06-21 18:23:04 +0200

    allowing send broadcast updates to every client - little refactor

--- backend/app.js
+++ backend/app.js
@@ -4,49 +4,41 @@ var WebSocketServer = require('ws').Server,
 
 var util  = require('util');
 
+wss.broadcast = function broadcast(data) {
+  var i = 0;
+  wss.clients.forEach(function each(client) {
+    console.log('sending to client ', i++);
+    client.send(data);
+  });
+};
+
 
 wss.on('connection', function connection(ws) {
-
-  console.log('connected!!',ws);
-
+  console.log('client connected!!',ws);
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
+});
 
-  var data = [ [ 1434585600000 , 10] , [ 1434672000000 , 256] ,[ 1434758400000 , 100] , [ 1434844800000 , 156] ];
-  var beginning = 0;
-  setInterval(function(){
-    try {
+var data = [ [ 1434585600000 , 10] , [ 1434672000000 , 256] ,[ 1434758400000 , 100] , [ 1434844800000 , 156] ];
+var beginning = 0;
+var eventObject = function(evt, object){
+  return {
+    event: evt,
+    data: object
+  };
+};
+
+setInterval(function(){
+  console.log('sending broadcast data');
+  try {
       beginning+=10;
       var result = _.map(data, function(e){
         return [e[0],e[1]+beginning];
       });
-      //console.log('sending data with factor:', beginning, result);
-      ws.send(JSON.stringify(eventObject('dau', result)));
+      wss.broadcast(JSON.stringify(eventObject('dau', result)));
     }catch(er){
       console.log(er);
     }
-  },1000);
 
-  var eventObject = function(evt, object){
-    return {
-      event: evt,
-      data: object
-    };
-  };
-});
-
-setInterval(function(){
-  console.log('sending broadcast data');
-  wss.broadcast = function broadcast(data) {
-    var i = 0;
-    wss.clients.forEach(function each(client) {
-      console.log('sending to client ', i++);
-      //client.send(data);
-    });
-  };
-  wss.broadcast(JSON.stringify({
-      event: 'daily',
-      data: [ [ 1434585600000 , 10] , [ 1434672000000 , 256] ,[ 1434758400000 , 100] , [ 1434844800000 , 156] ]
-    }));
 },2000);
